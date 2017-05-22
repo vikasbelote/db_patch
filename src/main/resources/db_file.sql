cae35767885c897c97686e40545dadbb3b9e2fd0
Bhaskar Belote <vikas.bhaskar.belote@accenture.com>
2017-05-22 18:28:03 +0530

    First commit

---
+++ src/main/resources/db_file.sql
@@ -0,0 +1,15 @@
+
+drop table my_test;
+
+create table my_test
+(
+  user_id int primary key
+  ,user_name varchar(20)
+);
+
+
+
+insert into my_test values(1,'vikas');
+insert into my_test values(2,'vikas');
+insert into my_test values(3,'vikas');
+insert into my_test values(4,'vikas');
